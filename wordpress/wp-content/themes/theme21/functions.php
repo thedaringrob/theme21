1cf532e53f1c61a85d24b9ab573137c93c570c34
Robert Crozier <crozierr2@gmail.com>
2018-09-06 18:42:15 -0400

    removed sidebar

--- wordpress/wp-content/themes/theme21/functions.php
+++ wordpress/wp-content/themes/theme21/functions.php
@@ -104,18 +104,18 @@ add_action( 'after_setup_theme', 'theme21_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function theme21_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'theme21' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'theme21' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'theme21_widgets_init' );
+// function theme21_widgets_init() {
+// 	register_sidebar( array(
+// 		'name'          => esc_html__( 'Sidebar', 'theme21' ),
+// 		'id'            => 'sidebar-1',
+// 		'description'   => esc_html__( 'Add widgets here.', 'theme21' ),
+// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 		'after_widget'  => '</section>',
+// 		'before_title'  => '<h2 class="widget-title">',
+// 		'after_title'   => '</h2>',
+// 	) );
+// }
+// add_action( 'widgets_init', 'theme21_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
